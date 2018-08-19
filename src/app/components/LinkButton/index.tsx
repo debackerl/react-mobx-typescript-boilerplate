eb568d4635c6a8498a3e1c48390d4f9c46dc7207
Laurent Debacker <debackerl gmail com>
2018-08-19 21:22:40 +0200

    Fix LinkButton

--- src/app/components/LinkButton/index.tsx
+++ src/app/components/LinkButton/index.tsx
@@ -1,23 +1,63 @@
 import * as React from 'react'
-import { withRouter, RouteComponentProps } from 'react-router'
+import * as PropTypes from 'prop-types';
+import { createLocation, LocationDescriptor } from "history";
 import { IButtonProps, Button } from '@blueprintjs/core';
+import * as style from './style.css';
 
 export interface ILinkButtonProps extends IButtonProps {
-  to: string;
+  target?: string;
+  replace?: boolean;
+  to: LocationDescriptor;
 }
 
-export const LinkButton = withRouter((props: ILinkButtonProps & RouteComponentProps<any>) => {
-  const { onClick, to, history } = props;
-  // TODO: only return properties from IButtonProps to spread operator
-  return (
-    <Button
-      {...props}
-      onClick={(event: React.MouseEvent<HTMLElement>) => {
-        if(onClick) onClick(event);
-        history.push(to);
-      }}
-    />
-  );
-});
+export class LinkButton extends React.PureComponent<ILinkButtonProps> {
+  static contextTypes: React.ValidationMap<any> = {
+    router: PropTypes.shape({
+      history: PropTypes.shape({
+        push: PropTypes.func.isRequired,
+        replace: PropTypes.func.isRequired,
+        createHref: PropTypes.func.isRequired
+      }).isRequired
+    }).isRequired
+  };
+
+  handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    if (this.props.onClick) this.props.onClick(event);
+
+    if (
+      !event.defaultPrevented &&
+      event.button === 0 &&
+      !this.props.target &&
+      !(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
+    ) {
+      event.preventDefault();
+
+      const { history } = this.context.router;
+
+      if (this.props.replace) {
+        history.replace(this.props.to);
+      } else {
+        history.push(this.props.to);
+      }
+    }
+  }
+
+  render() {
+    const { onClick, target, replace, to, ...subprops } = this.props;
+
+    const { history } = this.context.router;
+    const location =
+      typeof to === "string"
+        ? createLocation(to, null, null, history.location)
+        : to;
+    const href = history.createHref(location);
+
+    return (
+      <a href={href} className={style.normal}>
+        <Button {...subprops} onClick={this.handleClick} />
+      </a>
+    );
+  }
+}
 
 export default LinkButton;
